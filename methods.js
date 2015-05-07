9cb1645b8a2a9a46432828dd3bf992f84f24d8b8
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-08 00:22:39 +0200

    Fix save method to update only changed fields

--- methods.js
+++ methods.js
@@ -1,6 +1,13 @@
 Meteor.methods({
   '/member/save': function(data) {
-    var member = new Member(data);
+    var member;
+
+    if (data._id) {
+      member = Members.findOne(data._id);
+      member.set(data);
+    } else {
+      member = new Member(data);
+    }
 
     if (member.validate()) {
       return member.save();
