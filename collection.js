a48a9e66d580995b4eb4c34b87abb6f22178c6bf
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-31 15:26:35 +0200

    Update example to work with Astronomy 0.10.0

--- collection.js
+++ collection.js
@@ -15,24 +15,28 @@ User = Astro.Class({
     }
   },
   validators: {
-    firstName: Validators.and([
+    firstName: [
       Validators.required(),
-      Validators.minlen(3)
-    ]),
-    lastName: Validators.and([
+      Validators.string(),
+      Validators.minLength(3)
+    ],
+    lastName: [
       Validators.required(),
-      Validators.minlen(3)
-    ]),
-    email: Validators.and([
+      Validators.string(),
+      Validators.minLength(3)
+    ],
+    email: [
+      Validators.required(),
+      Validators.string(),
+      Validators.email(3),
       Validators.unique(),
+    ],
+    age: [
       Validators.required(),
-      Validators.email(3)
-    ]),
-    age: Validators.and([
-      Validators.required(),
+      Validators.number(),
       Validators.gte(18),
       Validators.lte(100)
-    ])
+    ]
   },
   behaviors: ['timestamp']
 });
