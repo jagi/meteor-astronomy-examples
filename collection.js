1f2e8853bd01102aea53b8993cd65dccf57dc4ef
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-06-08 00:07:55 +0200

    Test

--- collection.js
+++ collection.js
@@ -4,39 +4,70 @@ User = Astro.Class({
   name: 'User',
   collection: Users,
   fields: {
-    firstName: 'string',
-    lastName: 'string',
-    email: 'string',
-    age: 'number'
+    'object': {
+      type: 'object',
+      default: {}
+    },
+    'object.subobject': {
+      type: 'string',
+      default: 'sub'
+    },
+    'addresses': {
+      type: 'array',
+      default: []
+    },
+    'addresses.$': {
+      type: 'object',
+      default: {
+        state: '',
+        city: ''
+      }
+    },
+    'addresses.$.state': {
+      type: 'string'
+    },
+    'addresses.$.city': {
+      type: 'string'
+    },
+    'firstName': {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.minLength(3)
+      ]
+    },
+    'lastName': {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.minLength(3)
+      ]
+    },
+    'email': {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.email(3),
+        Validators.unique(),
+      ]
+    },
+    'age': {
+      type: 'number',
+      validators: [
+        Validators.required(),
+        Validators.number(),
+        Validators.gte(18),
+        Validators.lte(100)
+      ]
+    }
   },
   methods: {
     fullName: function() {
       return this.firstName + ' ' + this.lastName;
     }
   },
-  validators: {
-    firstName: [
-      Validators.required(),
-      Validators.string(),
-      Validators.minLength(3)
-    ],
-    lastName: [
-      Validators.required(),
-      Validators.string(),
-      Validators.minLength(3)
-    ],
-    email: [
-      Validators.required(),
-      Validators.string(),
-      Validators.email(3),
-      Validators.unique(),
-    ],
-    age: [
-      Validators.required(),
-      Validators.number(),
-      Validators.gte(18),
-      Validators.lte(100)
-    ]
-  },
-  behaviors: ['timestamp']
+  behaviors: ['timestamp', 'softremove']
 });
