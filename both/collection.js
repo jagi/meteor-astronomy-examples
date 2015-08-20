8f2d041f290e3dde4d68eff0921faae1e3d71195
jagi <luke.jagodzinski@gmail.com>
2015-08-20 16:42:12 +0200

    Testing embeded

---
+++ both/collection.js
@@ -0,0 +1,109 @@
+Address = Astro.Class({
+  name: 'Address',
+  fields: {
+    city: {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.minLength(3)
+      ]
+    },
+    state: {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.minLength(3)
+      ]
+    }
+  },
+  methods: {
+    where: function() {
+      return this.city + ', ' + this.state;
+    }
+  }
+});
+
+Phone = Astro.Class({
+  name: 'Phone',
+  fields: {
+    name: {
+      type: 'string'
+    },
+    number: {
+      type: 'string'
+    }
+  },
+  methods: {
+    call: function() {
+      console.log('Calling ' + this.name + ' phone: ' + this.number);
+    }
+  }
+});
+
+Users = new Mongo.Collection('users');
+
+User = Astro.Class({
+  name: 'User',
+  collection: Users,
+  embedOne: {
+    'address': {
+      class: 'Address'
+    }
+  },
+  embedMany: {
+    'phones': {
+      class: 'Phone'
+    }
+  },
+  fields: {
+    'firstName': {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.minLength(3)
+      ]
+    },
+    'lastName': {
+      type: 'string',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.minLength(3)
+      ]
+    },
+    'email': {
+      type: 'string',
+      default: 'example@mail.com',
+      validators: [
+        Validators.required(),
+        Validators.string(),
+        Validators.email(3),
+        Validators.unique(),
+      ]
+    },
+    'age': {
+      type: 'number',
+      validators: [
+        Validators.required(),
+        Validators.number(),
+        Validators.gte(18),
+        Validators.lte(100)
+      ]
+    }
+  },
+  methods: {
+    fullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    }
+  },
+  behaviors: {
+    timestamp: {},
+    slug: {
+      fieldName: 'firstName',
+      canUpdate: false
+    }
+  }
+});
