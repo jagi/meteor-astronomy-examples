0f594459eba759eec391c4757f5513944d14eaf1
jagi <luke.jagodzinski@gmail.com>
2015-09-22 10:09:36 +0200

    Don't format date when there is not birth date

--- both/collection.js
+++ both/collection.js
@@ -114,18 +114,20 @@ User = Astro.Class({
     formattedBirthDate: function() {
       var date = this.birthDate;
 
-      var year = date.getFullYear();
-      var month = date.getMonth() + 1;
-      var day = this.birthDate.getDate();
+      if (date) {
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
 
-      if (month < 10) {
-        month = '0' + month;
-      }
-      if (day < 10) {
-        day = '0' + day;
-      }
+        if (month < 10) {
+          month = '0' + month;
+        }
+        if (day < 10) {
+          day = '0' + day;
+        }
 
-      return year + '-' + month + '-' + day;
+        return year + '-' + month + '-' + day;
+      }
     }
   },
   behaviors: {
