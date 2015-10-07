6c5f4b02b0be0e2b10b5f17300948961078e04e5
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-10-07 21:54:14 +0200

    Adding and removing phones

--- client/templates.js
+++ client/templates.js
@@ -14,14 +14,31 @@ Template.User.events({
 
 Template.Form.events({
   'change input': function(e, tmpl) {
-    var doc = this;
+    var doc = this; // Instance of User, Phone or Address class.
 
-    var field = e.currentTarget;
-    doc.set(field.id, field.value);
-    doc.validate(field.id);
+    // Get an input which value was changed.
+    var input = e.currentTarget;
+    // Set a value of a field in an instance of User, Phone or Address class.
+    doc.set(input.id, input.value);
+    // Validate given field.
+    doc.validate(input.id);
+  },
+  'click [name=addPhone]': function(e, tmpl) {
+    var user = tmpl.data.user.get();
+    // FIXME: It should be:
+    // user.push('phones', new Phone());
+    // However, it will not work because of trying to modify the same field with
+    // two different modifiers. I have to implement modifiers merging.
+    user.phones.push(new Phone());
+    tmpl.data.user.set(user);
+  },
+  'click [name=removePhone]': function(e, tmpl) {
+    var user = tmpl.data.user.get();
+    user.pull('phones', this);
+    tmpl.data.user.set(user);
   },
   'click [name=save]': function(e, tmpl) {
-    var user = this;
+    var user = tmpl.data.user.get();
 
     if (user.validate()) {
       Meteor.call('/user/save', user, function(err) {
